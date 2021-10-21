e47a7e42c12a7668390e0dd1dd3c78fb492ac2be
unknown <raveen.anand@whitehatjr.com>
2021-10-21 19:34:59 +0530

    v2

--- Form Validation/myscript.js
+++ Form Validation/myscript.js
@@ -1,16 +1,27 @@
 function validate()
 {
     //1. Take input from name and contact fields , store them in variables
-    
+    var name = document.getElementById("");
+    var phone = document.getElementById("");
 
-    //2. calculate length of contact number and store i a variable named  "digitCount"
-   
+    //2. calculate length of contact number and store it in a variable named "count"
+    var count= phone.length;
     
-    //3. if name input is blank, display the 'errorName' span, else hide it
-   
-    
-    //3. if digitCount is not equal to 10, display the 'errorContact' span, else hide it
-    
+    //3. if name input is blank, display the 'err1' element, else hide it
+    if(){
+        document.getElementById("");
+    }
+    else{
+        document.getElementById("");
+    }
+
+    //4. if count is not equal to 10, display the 'err2' element, else hide it*/
+    if(){
+        document.getElementById("");
+    }
+    else{
+        document.getElementById("");
+    }
     
 }
 
